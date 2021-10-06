1d5a9854b4c966e6b52d6ee99ea2cf731ce60aa8
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-10-06 17:41:30 +0300

    prepare task_3

---
+++ src/OOP_2/Task_3/Car.java
@@ -0,0 +1,27 @@
+package OOP_2.Task_3;
+
+public class Car {
+    private int year;
+    private double speed;
+    private int weight;
+    private String color;
+    public Car(){}
+    public Car(int year){
+        this.year = year;
+    }
+    public Car(int year, double speed){
+        this.year = year;
+        this.speed = speed;
+    }
+    public Car(int year, double speed, int weight){
+        this.year = year;
+        this.speed = speed;
+        this.weight = weight;
+    }
+    public Car(int year, double speed, int weight, String color){
+        this.year = year;
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+    }
+}
---
+++ src/OOP_2/Task_3/Main.java
@@ -0,0 +1,11 @@
+package OOP_2.Task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car(2014);
+        Car car3 = new Car(2015, 200);
+        Car car4 = new Car(2011,250, 1400);
+        Car car5 = new Car(2020, 300, 1500, "Red");
+    }
+}
